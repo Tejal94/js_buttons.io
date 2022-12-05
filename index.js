8ff9f1d14679b9d4ff37933b1192394016f22d70
Tejal94 <tejalshigwan@gmail.com>
2022-12-05 14:07:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function india(){
+    document.getElementById('map').src="/images/india.jpg";
+}
+
+function maharashtra(){
+    document.getElementById('map').src="/images/maharashtra.jpg";
+}
+
+function andhra(){
+    document.getElementById('map').src="/images/andhra.jpg";
+}
+
+function karnataka(){
+    document.getElementById('map').src="/images/karnataka.jpg";
+}
+
+function kashmir(){
+    document.getElementById('map').src="/images/kashmir.jpg";
+}
+
+function kerala(){
+    document.getElementById('map').src="/images/kerala.jpg";
+}
+
+function mizoram(){
+    document.getElementById('map').src="/images/mizoram.jpg";
+}
+
+function odisha(){
+    document.getElementById('map').src="/images/odisha.jpg";
+}
+
+function tamil(){
+    document.getElementById('map').src="/images/Tamilnadu.jpg";
+}
+
+function up(){
+    document.getElementById('map').src="/images/up.jpg";
+}
+
+function jharkhand(){
+    document.getElementById('map').src="/images/jharkhand.jpg";
+}
